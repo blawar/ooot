0428729466261ab069290d97e194f8a05aa6f675
Torphedo <ahiatt.cosgrove@gmail.com>
2022-02-03 20:25:11 -0500

    Only use script names in batchfile

--- setup_assets.bat
+++ setup_assets.bat
@@ -1,17 +1,17 @@
 @echo off
-python -m pip install tqdm
-python -m pip install pathtools
-python -m pip install multiprocessing
-python fixbaserom.py
-python extract_baserom.py
-python extract_assets.py
+pip3 install tqdm
+pip3 install pathtools
+pip3 install multiprocessing
+fixbaserom.py
+extract_baserom.py
+extract_assets.py
 
 python tools\convert_assets.py
 
 mkdir build\assets\text
-python tools\msgenc.py assets\text\charmap.txt assets\text\message_data.h build\assets\text\message_data.enc.h
-python tools\msgenc.py assets\text\charmap.txt assets\text\message_data_staff.h build\assets\text\message_data_staff.enc.h
+tools\msgenc.py assets\text\charmap.txt assets\text\message_data.h build\assets\text\message_data.enc.h
+tools\msgenc.py assets\text\charmap.txt assets\text\message_data_staff.h build\assets\text\message_data_staff.enc.h
 
-python tools\extract_missing_assets.py
-python tools\create_luts.py
-python tools\mark_segments.py
+tools\extract_missing_assets.py
+tools\create_luts.py
+tools\mark_segments.py
