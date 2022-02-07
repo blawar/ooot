0fe9f0862adf109ab6de45306271e18275399932
HungryBananaSalad <>
2022-02-07 03:13:02 +0100

    Refactor asset conversion and add multithreading

--- tools/convert_assets.py
+++ tools/convert_assets.py
@@ -1,158 +1,60 @@
 #!/usr/bin/python3
 
-import os.path
-from pathlib import Path
 import os
-import queue
-import threading
-import tqdm
-from multiprocessing import cpu_count
+from pathlib import Path
+from itertools import repeat
+from tqdm import tqdm
+from concurrent.futures import ThreadPoolExecutor
 
-basedir = os.path.abspath(os.path.join(os.path.dirname(os.path.realpath(__file__.replace('\\', '/'))), '../'))
+zapd = Path("tools/ZAPD/ZAPD.out")
 
+def worker(path, type):
+    rel = path.with_suffix('')
+    out = 'build' / path
+    cmd = None
+    # Use different commands depending on file type
+    if type == 'jpg':
+        cmd = f"{zapd} bren -i {rel}.jpg -o {out}.jpg.inc.c"
+    elif type == 'bin':
+        cmd = f"{zapd} bblb -i {rel}.bin -o {out}.bin.inc.c"
+    elif type == 'png':
+        out = Path(f"build/{rel}.inc.c")
+        type = Path(rel).suffix[1:]
+        cmd = f"{zapd} btex -tt {type} -i {rel}.png -o {out}"
 
-thread_count = 1 # cpu_count()
+    # Create folder before running of command. Otherwise it silently fails without converting
+    Path.mkdir(out.parent, parents=True, exist_ok = True)
+    os.system(cmd)
 
-def info(m):
-	tqdm.tqdm.write(m)
+def main():
+    print("Start converting assets")
 
-def cleanPath(path):
-	return str(path).replace('\\', '/').replace('/', os.sep)
+    # Gather all asset paths recursively in their respective lists
+    pngs = []
+    for path in Path('assets').rglob('*.png'):
+        pngs.append(path)
 
-def getRelativePath(path):
-	return cleanPath(path[len(basedir):].strip('/').strip('\\'))
+    jpgs = []
+    for path in Path('assets').rglob('*.jpg'):
+        jpgs.append(path)
 
-def shellquote(s):
-	return "'" + s.replace("'", "'\\''") + "'"
-	
-def ensureDirectory(path):
-	try:
-		p = Path(path)
-		os.makedirs(p.parent)
-	except:
-		pass
+    bins = []
+    for path in Path('assets').rglob('*.bin'):
+        bins.append(path)
 
-try:
-	os.makedirs(cleanPath('assets/build/text/'))
-except:
-	pass
-		
-zapd = None
-for path in Path(os.path.abspath('tools\ZAPD')).rglob('ZAPD.out'):
-	zapd = getRelativePath(os.path.abspath(str(path)))
-	
-if not zapd:
-	raise IOError('ZAPD.out not found')
+    # Multithreaded conversion of all assets with progress bar
+    # Calls generalized worker by passing type
+    thread_count = os.cpu_count() or 1
+    with ThreadPoolExecutor(max_workers = thread_count) as executor:
+        with tqdm(total = len(pngs) + len(jpgs) + len(bins)) as progress:
+            for result in executor.map(worker, pngs, repeat('png')):
+                progress.update()
+            for result in executor.map(worker, jpgs, repeat('jpg')):
+                progress.update()
+            for result in executor.map(worker, bins, repeat('bin')):
+                progress.update()
 
-'''
-searchdir = os.path.abspath('assets/xml')
-for path in Path(searchdir).rglob('*.xml'):
-	rel = cleanPath(str(path)[len(searchdir):-4].strip('/').strip('\\'))
-	out = getRelativePath(os.path.abspath(os.path.join('assets/', rel)))
-	ensureDirectory(out)
-	cmd = '%s e -i %s -b baserom\ -o %s -osf %s -gsf 1 -rconf tools\ZAPDConfigs\MqDbg\Config.xml' % (zapd, getRelativePath(str(path)), out, out)
-	info(cmd)
-	os.system(cmd)
-'''
+    print("Finished converting assets")
 
-threads = []
-q = queue.LifoQueue()
-searchdir = os.path.abspath('assets')
-
-class Item:
-	def __init__(self, path, rel, out):
-		self.path = path
-		self.rel = rel
-		self.out = out
-		
-	def isStale(self):
-		return os.path.exists(self.out)
-
-def pngWorker(q, status):
-	while not q.empty():
-		try:
-			item = q.get(block=False)
-			
-			if item is None:
-				return
-
-			path = item.path
-			rel = item.rel
-			out = item.out
-			#rel = cleanPath(str(path)[len(basedir):-4].strip('/').strip('\\'))
-			#out = getRelativePath(os.path.abspath(os.path.join('build', rel)))
-
-			type = rel.split('.')[-1]
-			ensureDirectory(out)
-			cmd = '%s btex -tt %s -i %s.png -o %s' % (zapd, type, rel, out)
-			info(cmd)
-			os.system(cmd)
-			status.update(1)
-		except queue.Empty as e:
-			return
-		except BaseException as e:
-			info('PNG WORKER EXCEPTION: ' + str(e))
-			#traceback.print_exc(file=sys.stdout)
-		q.task_done()
-
-for path in Path(searchdir).rglob('*.png'):
-	rel = cleanPath(str(path)[len(basedir):-4].strip('/').strip('\\'))
-	out = getRelativePath(os.path.abspath(os.path.join('build', rel))) + '.inc.c'
-	
-	item = Item(path, rel, out)
-	if not item.isStale():
-		q.put(item)
-
-
-with tqdm.tqdm(total=q.qsize()) as status:
-	if thread_count == 1:
-		pngWorker(q, status)
-	else:
-		for i in range(thread_count):
-			t = threading.Thread(target=pngWorker, args=[q, status])
-			t.daemon = True
-			t.start()
-			threads.append(t)
-			
-		info('q.join start')
-		q.join()
-		info('q.join end')
-		for i in range(thread_count):
-			q.put(None)
-			
-		info('waiting for threads to finish')
-			
-		i = 0
-		for t in threads:
-			t.join()
-			i = i + 1
-			info('joined thread %d' % i)
-
-	
-'''
-	rel = cleanPath(str(path)[len(basedir):-4].strip('/').strip('\\'))
-	out = getRelativePath(os.path.abspath(os.path.join('build', rel)))
-	type = rel.split('.')[-1]
-	ensureDirectory(out)
-	cmd = '%s btex -tt %s -i %s.png -o %s.inc.c' % (zapd, type, rel, out)
-	info(cmd)
-	os.system(cmd)
-'''
-	
-searchdir = os.path.abspath('assets')
-for path in Path(searchdir).rglob('*.jpg'):
-	rel = cleanPath(str(path)[len(basedir):-4].strip('/').strip('\\'))
-	out = getRelativePath(os.path.abspath(os.path.join('build', rel)))
-	ensureDirectory(out)
-	cmd = '%s bren -i %s.jpg -o %s.jpg.inc.c' % (zapd, rel, out)
-	info(cmd)
-	os.system(cmd)
-	
-searchdir = os.path.abspath('assets')
-for path in Path(searchdir).rglob('*.bin'):
-	rel = cleanPath(str(path)[len(basedir):-4].strip('/').strip('\\'))
-	out = getRelativePath(os.path.abspath(os.path.join('build', rel)))
-	ensureDirectory(out)
-	cmd = '%s bblb -i %s.bin -o %s.bin.inc.c' % (zapd, rel, out)
-	info(cmd)
-	os.system(cmd)
+if __name__ == "__main__":
+    main()
