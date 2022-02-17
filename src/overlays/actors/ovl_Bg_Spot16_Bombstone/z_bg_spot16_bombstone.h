e1def7bdddf454c1a2f4d49dea551450b4d5c56a
Blake Warner <blake@null3d.com>
2022-02-17 00:32:42 -0500

    z_bg_spot16_bombstone documentation

--- src/overlays/actors/ovl_Bg_Spot16_Bombstone/z_bg_spot16_bombstone.h
+++ src/overlays/actors/ovl_Bg_Spot16_Bombstone/z_bg_spot16_bombstone.h
@@ -14,7 +14,7 @@ struct BgSpot16Bombstone {
     /* 0x0000 */ Actor actor;
     /* 0x014C */ BgSpot16BombstoneActionFunc actionFunc;
     /* 0x0150 */ Gfx* dList;
-    /* 0x0154 */ s16 unk_154;
+    /* 0x0154 */ Timer counter;
     /* 0x0156 */ s16 switchFlag;
     /* 0x0158 */ s16 unk_158;
     /* 0x015C */ f32 sinRotation;
@@ -22,8 +22,8 @@ struct BgSpot16Bombstone {
     /* 0x0164 */ ColliderJntSph colliderJntSph;
     /* 0x0184 */ ColliderJntSphElement colliderElements[1];
     /* 0x01C4 */ ColliderCylinder colliderCylinder;
-    /* 0x0210 */ s16 unk_210;
-    /* 0x0212 */ s16 unk_212;
+    /* 0x0210 */ s16 rotateSpeedX;
+    /* 0x0212 */ s16 rotateSpeedZ;
     /* 0x0214 */ s32 bombiwaBankIndex;
 }; 
 
