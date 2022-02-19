6fd099abf442f28736a5a7b2a8d7b2bb3d55d7fc
DaMarkov <damarkov@gmail.com>
2022-02-19 16:09:10 +0100

    Improved the code. Added #include "gfx_align.h", removed the N64_VERSION flag

--- src/code/z_fbdemo_circle.cpp
+++ src/code/z_fbdemo_circle.cpp
@@ -4,12 +4,7 @@
 #include "z64transition.h"
 #include "z64audio.h"
 #include "sfx.h"
-
-extern "C"
-{
-	u64 gfx_width();
-	u64 gfx_height();
-}
+#include "gfx_align.h"
 
 // unused
 Gfx sCircleNullDList[] = {
@@ -158,12 +153,8 @@ void TransitionCircle_Draw(void* thisx, Gfx** gfxP) {
     // These variables are a best guess based on the other transition types.
     f32 tPos = 0.0f;
     f32 rot = 0.0f;
-#ifdef N64_VERSION
-    f32 scale = 14.8f;
-#else
     const float correction_factor = (gfx_width() * 3.0f) / (gfx_height() * 4.0f); // Should be 1 on a 4:3 display
     f32 scale = 14.8f * correction_factor;
-#endif
 
     modelView = pthis->modelView[pthis->frame];
 
--- src/overlays/actors/ovl_Oceff_Wipe3/z_oceff_wipe3.cpp
+++ src/overlays/actors/ovl_Oceff_Wipe3/z_oceff_wipe3.cpp
@@ -15,12 +15,7 @@
 #include "def/z_lib.h"
 #include "def/z_parameter.h"
 #include "def/z_rcp.h"
-
-extern "C"
-{
-	u64 gfx_width();
-	u64 gfx_height();
-}
+#include "gfx_align.h"
 
 #define FLAGS (ACTOR_FLAG_4 | ACTOR_FLAG_25)
 
@@ -110,12 +105,8 @@ void OceffWipe3_Draw(Actor* thisx, GlobalContext* globalCtx) {
     func_80093D84(globalCtx->state.gfxCtx);
 
     Matrix_Translate(eye.x + vec.x, eye.y + vec.y, eye.z + vec.z, MTXMODE_NEW);
-#ifdef N64_VERSION
-    Matrix_Scale(0.1f, 0.1f, 0.1f, MTXMODE_APPLY);
-#else
     const float correction_factor = (gfx_width() * 3.0f) / (gfx_height() * 4.0f); // Should be 1 on a 4:3 display
     Matrix_Scale(0.1f * correction_factor, 0.1f, 0.1f, MTXMODE_APPLY);
-#endif
     func_800D1FD4(&globalCtx->billboardMtxF);
     Matrix_Translate(0.0f, 0.0f, -z, MTXMODE_APPLY);
 
