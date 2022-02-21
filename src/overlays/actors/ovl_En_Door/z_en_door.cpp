cfb7c151e3815426db8323a127fb26b6eeb5ca1c
DaMarkov <DaMarkovZED@gmail.com>
2022-02-21 03:01:28 +0100

    Fixed forest temple doors again.

--- src/overlays/actors/ovl_En_Door/z_en_door.cpp
+++ src/overlays/actors/ovl_En_Door/z_en_door.cpp
@@ -65,7 +65,7 @@ static EnDoorInfo sDoorInfo[] = {
     { SCENE_MIZUSIN, 2, OBJECT_MIZU_OBJECTS },
     { SCENE_HAKADAN, 3, OBJECT_HAKA_DOOR },
     { SCENE_HAKADANCH, 3, OBJECT_HAKA_DOOR },
-#ifdef N64_VERSION
+#ifndef N64_VERSION
     { SCENE_BMORI1, 0, OBJECT_GAMEPLAY_KEEP },//Doors in the forest temple. Might be incorrect
 #endif
     // KEEP objects should remain last and in pthis order
