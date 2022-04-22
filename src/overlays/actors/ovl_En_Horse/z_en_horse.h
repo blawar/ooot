18074e02645c63d6335d1f6b66197172c472fa20
Blake Warner <blake@null3d.com>
2022-04-21 21:34:55 -0400

    fixed weird ingo turning

--- src/overlays/actors/ovl_En_Horse/z_en_horse.h
+++ src/overlays/actors/ovl_En_Horse/z_en_horse.h
@@ -6,6 +6,15 @@
 #include "z64actor.h"
 #include "z64math.h"
 
+enum Hoof : u16
+{
+	HOOF_NONE = 0,
+	FRONT_RIGHT = 1,
+	FRONT_LEFT = 2,
+	REAR_RIGHT = 4,
+	REAR_LEFT = 8
+};
+
 enum EnHorseAction
 {
 	/* 0  */ ENHORSE_ACT_FROZEN,
@@ -205,7 +214,7 @@ struct EnHorse
 	/* 0x03C4 */ s16 bridgeJumpRelAngle;
 	/* 0x03C6 */ s16 unk_3C6; // pad
 				  // sub struct?
-	/* 0x03C8 */ u16 dustFlags;
+	/* 0x03C8 */ Hoof dustFlags;
 	/* 0x03CC */ Vec3f frontRightHoof;
 	/* 0x03D8 */ Vec3f frontLeftHoof;
 	/* 0x03E4 */ Vec3f backRightHoof;
