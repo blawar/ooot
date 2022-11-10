04919e884a919f9da170ebfac62e8ed33c41d4f7
frizzle101101 <aaronfritz96@gmail.com>
2022-11-10 00:27:34 -0500

    fix the faries effect in koriki forest

--- src/overlays/actors/ovl_Object_Kankyo/z_object_kankyo.h
+++ src/overlays/actors/ovl_Object_Kankyo/z_object_kankyo.h
@@ -11,20 +11,20 @@ typedef void (*ObjectKankyoActionFunc)(struct ObjectKankyo*, GlobalContext*);
 struct ObjectKankyoEffect
 {
 	/* 0x00 */ u8 state;
-	/* 0x04 */ VecPos pos; // relative to base
-	/* 0x10 */ Vec3f prevPos;
-	/* 0x1C */ Vec3f base;
-	/* 0x28 */ Vec3f dirPhase; // input to sin/cos for movement direction
+	/* 0x04 */ VecCounterF pos; // relative to base
+	/* 0x10 */ VecCounterF prevPos;
+	/* 0x1C */ VecCounterF base;
+	/* 0x28 */ VecCounterF dirPhase; // input to sin/cos for movement direction
 	/* 0x34 */ f32 speed;
 	/* 0x38 */ f32 targetSpeed;
-	/* 0x3C */ Timer alphaTimer;
-	/* 0x3E */ u16 angle;
-	/* 0x40 */ u8 alpha;
+	/* 0x3C */ TimerU16 alphaTimer;
+	/* 0x3E */ TimerU16 angle;
+	/* 0x40 */ TimerU8 alpha;
 	/* 0x44 */ f32 size;
 	/* 0x48 */ u16 angleVel;
 	/* 0x4A */ u16 flightRadius;
 	/* 0x4C */ f32 amplitude;
-	/* 0x50 */ Timer timer;
+	/* 0x50 */ TimerU16 timer;
 };
 
 struct ObjectKankyo
